962fe1ad6cea7d3e7d0d6f5c529345f05fef73a1
Fernando <fernando@raffaelles-mbp.fritz.box>
2019-03-05 21:10:20 +0100

    Initial Commit

---
+++ ProductRxSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductRxSample
+//
+//  Created by Fernando on 05.03.19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
